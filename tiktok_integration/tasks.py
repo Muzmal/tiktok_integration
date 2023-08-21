48622b44f79f55d5f36ef40040d7d008b763dc10
Muzmal <Muzammal_ckj@yahoo.com>
2023-08-21 12:26:08 +0500

    Update tasks.py

--- tiktok_integration/tasks.py
+++ tiktok_integration/tasks.py
@@ -4,5 +4,4 @@ from tiktok_integration.zaviago_tiktok.create_client import CreateTiktokClient
 def daily():
     tiktokClient = CreateTiktokClient()
     tiktokClient.refreshToken()
-    print("Cron job is working")
     return
