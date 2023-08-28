d7ff1efa0348a0b0c0276564eeba9be4945a334e
Muzmal <Muzammal_ckj@yahoo.com>
2023-08-28 17:12:59 +0500

    Update tiktok_with_erpnext.js

--- tiktok_integration/zaviago_tiktok/doctype/tiktok_with_erpnext/tiktok_with_erpnext.js
+++ tiktok_integration/zaviago_tiktok/doctype/tiktok_with_erpnext/tiktok_with_erpnext.js
@@ -89,7 +89,7 @@ frappe.ui.form.on('Tiktok with ERPnext', {
 			freeze: true,
 			freeze_message: "Fetching Products...",
 			callback:function(r){
-				console.log("called1")
+				console.log(r)
 			}
 			
 		}); 
