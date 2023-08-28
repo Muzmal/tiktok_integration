01ba5a3127d63466e06675866ae9d8b8901bdb3f
Muzmal <Muzammal_ckj@yahoo.com>
2023-08-28 16:09:18 +0500

    Update tiktok_with_erpnext.py

--- tiktok_integration/zaviago_tiktok/doctype/tiktok_with_erpnext/tiktok_with_erpnext.py
+++ tiktok_integration/zaviago_tiktok/doctype/tiktok_with_erpnext/tiktok_with_erpnext.py
@@ -399,8 +399,8 @@ class handleTiktokRequests:
 				if( ifExist == None ):	
 					tiktokProduct=save_data.fetchProduct( product['id'],False )
 					self.saveTiktokProduct( tiktokProduct )
-					return  product['id']
-					raise Exception(  product['id'] )
+				return  product['id']
+				raise Exception(  product['id'] )
 				
 					
 		else:
